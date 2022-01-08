d9548ac8b49aafbf6f97d8668f71bfacc7e57d5d
Dan Wallis <mrdanwallis@gmail.com>
2022-01-08 19:25:47 +0000

    Allow marking entries as ignored

---
+++ Block/Adminhtml/Edit/IgnoreButton.php
@@ -0,0 +1,33 @@
+<?php
+namespace Fredden\JavaScriptErrorReporting\Block\Adminhtml\Edit;
+
+use Magento\Backend\Ui\Component\Control\DeleteButton as MagentoDeleteButton;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Escaper;
+use Magento\Framework\UrlInterface;
+
+class IgnoreButton extends MagentoDeleteButton
+{
+    public function __construct(
+        RequestInterface $request,
+        UrlInterface $urlBuilder,
+        Escaper $escaper
+    ) {
+        parent::__construct(
+            $request,
+            $urlBuilder,
+            $escaper,
+            $confirmationMessage = 'Are you sure you want to hide this error?',
+            $idFieldName = 'id',
+            $deleteRoutePath = '*/*/ignoreHash',
+            $sortOrder = 25
+        );
+    }
+
+    public function getButtonData()
+    {
+        $data = parent::getButtonData();
+        $data['label'] = __('Ignore');
+        return $data;
+    }
+}
---
+++ Controller/Adminhtml/JavaScriptErrorReporting/IgnoreHash.php
@@ -0,0 +1,51 @@
+<?php
+namespace Fredden\JavaScriptErrorReporting\Controller\Adminhtml\JavaScriptErrorReporting;
+
+use Exception;
+use Fredden\JavaScriptErrorReporting\Model\EventFactory;
+use Fredden\JavaScriptErrorReporting\Scope\Config;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+class IgnoreHash extends Action implements HttpPostActionInterface
+{
+    const ADMIN_RESOURCE = 'Fredden_JavaScriptErrorReporting::ignore';
+
+    protected $config;
+    protected $eventFactory;
+
+    public function __construct(
+        Context $context,
+        Config $config,
+        EventFactory $eventFactory
+    ) {
+        parent::__construct($context);
+        $this->config = $config;
+        $this->eventFactory = $eventFactory;
+    }
+
+    public function execute()
+    {
+        $id = (int) $this->getRequest()->getParam('id');
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        if ($id) {
+            try {
+                $event = $this->eventFactory->create();
+                $event->load($id);
+
+                $this->config->addHashToIgnore($event->getHash());
+
+                $this->messageManager->addSuccessMessage(__('The event has been hidden from the summary table.'));
+            } catch (Exception $e) {
+                $this->messageManager->addErrorMessage($e->getMessage());
+            }
+        } else {
+            $this->messageManager->addErrorMessage(__('Unable to find that event to hide.'));
+        }
+
+        return $resultRedirect->setPath('*/*/statistics');
+    }
+}
