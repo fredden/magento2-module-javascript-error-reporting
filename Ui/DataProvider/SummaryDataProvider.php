6943bee75f25a6798889e1b624f09c50048b6e6a
Dan Wallis <mrdanwallis@gmail.com>
2022-10-19 16:59:04 +0100

    Reduce duplicate code

--- Ui/DataProvider/SummaryDataProvider.php
+++ Ui/DataProvider/SummaryDataProvider.php
@@ -4,9 +4,8 @@ namespace Fredden\JavaScriptErrorReporting\Ui\DataProvider;
 use Fredden\JavaScriptErrorReporting\Model\ResourceModel\Event\CollectionFactory;
 use Fredden\JavaScriptErrorReporting\Scope\Config;
 use Magento\Framework\DB\Select;
-use Magento\Ui\DataProvider\AbstractDataProvider;
 
-class SummaryDataProvider extends AbstractDataProvider
+class SummaryDataProvider extends EventDataProvider
 {
     public function __construct(
         $name,
@@ -17,17 +16,11 @@ class SummaryDataProvider extends AbstractDataProvider
         array $meta = [],
         array $data = []
     ) {
-        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
-        $this->collection = $collectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $config, $collectionFactory, $meta, $data);
 
         $this->collection->addExpressionFieldToSelect('count', 'COUNT(event_id)', '');
         $this->collection->addExpressionFieldToSelect('first', 'MIN(created_at)', '');
         $this->collection->addExpressionFieldToSelect('last', 'MAX(created_at)', '');
-
-        $ignoredHashes = $config->getIgnoredHashes();
-        if ($ignoredHashes) {
-            $this->collection->getSelect()->where('hash NOT IN (?)', $ignoredHashes);
-        }
 
         $this->collection->getSelect()->group('hash');
         $this->collection->getSelect()->order('count ' . Select::SQL_DESC);
