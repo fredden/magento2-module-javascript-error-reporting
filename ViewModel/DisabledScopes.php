4b303bdff27069bcd7888b70632f7746f04254f2
Dan Wallis <mrdanwallis@gmail.com>
2022-11-01 23:03:01 +0000

    Notify administrators when module is disabled

---
+++ ViewModel/DisabledScopes.php
@@ -0,0 +1,35 @@
+<?php
+namespace Fredden\JavaScriptErrorReporting\ViewModel;
+
+use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class DisabledScopes implements ArgumentInterface
+{
+    protected $configCollectionFactory;
+
+    public function __construct(
+        CollectionFactory $configCollectionFactory
+    ) {
+        $this->configCollectionFactory = $configCollectionFactory;
+    }
+
+    public function getDisabledScopes(): array
+    {
+        $collection = $this->configCollectionFactory->create();
+        $collection->addFieldToFilter('path', ['eq' => 'system/fredden_javascript_error_reporting/enabled']);
+        $collection->addFieldToFilter('value', ['eq' => '0']);
+
+        $result = [];
+
+        foreach ($collection as $configValue) {
+            /** @var \Magento\Framework\App\Config\Value $configValue */
+            $scope = rtrim($configValue->getScope(), 's');
+            $scopeId = $configValue->getScopeId();
+
+            $result[] = [$scope, $scopeId];
+        }
+
+        return $result;
+    }
+}
